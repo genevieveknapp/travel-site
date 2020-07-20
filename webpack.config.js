08ef3f4635d21c780affb05ec21d61ec574e7778
genevieveknapp <genevieve.knapp@gmail.com>
2020-07-20 14:35:53 -0500

    Bumped Lodash version

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,17 @@ let config = {
     plugins: [new HtmlWebpackPlugin({filename: 'index.html', template: './app/index.html'})],
     module: {
         rules: [
-            cssConfig
+            cssConfig,
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env']
+                    }
+                }
+            }
         ]
     }
 }
@@ -56,16 +66,6 @@ if (currentTask == 'dev') {
 }
 
 if (currentTask == 'build') {
-    config.module.rules.push({
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env']
-            }
-        }
-    })
     cssConfig.use.unshift(MiniCssExtractPlugin.loader)
     postCSSPlugins.push(require('cssnano'))
     config.output = {
